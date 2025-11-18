fb78d3ec9ff9b0a7201412cfc76042f8d9572dd9
Bruno Reis <bruno.reis.r6s@gmail.com>
2025-11-18 22:37:06 +0000

    pessoa classe

---
+++ classes.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+
+#Classe da Pessoa
+class Pessoa(ABC):
+    def __init__(self, primeiro_nome, sobrenome, email, data_nascimento,
+                 documento, id_pessoa, data_cadastro):
+        self._primeiro_nome = primeiro_nome
+        self._sobrenome = sobrenome
+        self._email = email
+        self._data_nascimento = data_nascimento
+        self._documento = documento
+        self._id_pessoa = id_pessoa
+        self._data_cadastro = data_cadastro
+        self._telefones = []  # relação 1,n
+
+    @property
+    def email(self):
+        return self._email
+
+    @abstractmethod
+    def exibir_informacoes(self):
+        pass
+
+    def adicionar_telefone(self, telefone):
+        self._telefones.append(telefone)
+
